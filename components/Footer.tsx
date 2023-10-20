427ec017314ec6ae0ac55073b210d207512b5a8f
nuhptr <nugrahaadi733@gmail.com>
2023-10-20 16:39:10 +0700

    comp: add navbar and footre

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,50 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import { footerLinks } from "@/constant"
+
 export default function Footer() {
-   return <div>Footer</div>
+   return (
+      <footer className="flex flex-col mt-5 border-t border-gray-100 text-black-100">
+         <div className="flex flex-wrap justify-between gap-5 px-6 py-10 max-md:flex-col sm:px-16">
+            <div className="flex flex-col items-start justify-start gap-6">
+               <Image
+                  src={"/logo.svg"}
+                  alt="Logo Drivecar Showroom"
+                  width={118}
+                  height={18}
+                  className="object-contain"
+               />
+               <p className="text-base leading-relaxed text-gray-700">
+                  Drivecar 2023 <br />
+                  All right reserved &copy;
+               </p>
+            </div>
+            <div className="footer__links">
+               {footerLinks.map((link) => (
+                  <div key={link.title} className="footer__link">
+                     <h3 className="font-bold">{link.title}</h3>
+                     {link.links.map((item) => (
+                        <Link key={item.title} href={item.url} className="text-gray-500">
+                           {item.title}
+                        </Link>
+                     ))}
+                  </div>
+               ))}
+            </div>
+         </div>
+
+         <div className="flex flex-wrap items-center justify-between px-6 py-10 mt-10 border-t border-gray-100 sm:px-16">
+            <p>@2023 Drivecar. All Right Reserved</p>
+            <div className="footer__copyrights-link">
+               <Link href={"/"} className="text-gray-500">
+                  Privacy Policy
+               </Link>
+               <Link href={"/"} className="text-gray-500">
+                  Terms of use
+               </Link>
+            </div>
+         </div>
+      </footer>
+   )
 }
