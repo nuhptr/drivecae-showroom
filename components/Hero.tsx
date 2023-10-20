a9901cb3697144210504a59d032e6dfe8351cb48
nuhptr <nugrahaadi733@gmail.com>
2023-10-20 23:54:05 +0700

    comp: update Hero code

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,9 +11,7 @@ export default function Hero() {
       <div className="hero">
          <div className="flex-1 pt-36 padding-x">
             <h1 className="hero__title">Find, book, or rent a car -- quickly and easily!</h1>
-            <p className="hero__subtitle">
-               Streamline your car rental experience with our effortless booking process.
-            </p>
+            <p className="hero__subtitle">Streamline your car rental experience with our effortless booking process.</p>
             <CustomButton
                title="Explore Card"
                containerStyles="bg-primary-blue text-white rounded-full mt-10"
@@ -24,9 +22,8 @@ export default function Hero() {
          <div className="hero__image-container">
             <div className="hero__image">
                <Image src={"/hero.png"} alt="Hero" fill className="object-contain" />
-
-               <div className="hero__image-overlay" />
             </div>
+            <div className="hero__image-overlay" />
          </div>
       </div>
    )
