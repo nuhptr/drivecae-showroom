4e3ce85559373166505d3856303adb6ecd713e76
nuhptr <nugrahaadi733@gmail.com>
2023-10-21 13:08:38 +0700

    comp: custom-button update code

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -13,10 +13,27 @@ type ICustomButtonProps = {
    handleClick?: MouseEventHandler<HTMLButtonElement>
 }
 
-export default function CustomButton({ title, containerStyles, handleClick, btnType }: ICustomButtonProps) {
+export default function CustomButton({
+   title,
+   containerStyles,
+   textStyles,
+   isDisabled,
+   rightIcon,
+   handleClick,
+   btnType,
+}: ICustomButtonProps) {
    return (
-      <button disabled={false} type={btnType} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
-         <span className={`flex-1`}>{title}</span>
+      <button
+         disabled={isDisabled}
+         type={btnType || "button"}
+         className={`custom-btn ${containerStyles}`}
+         onClick={handleClick}>
+         <span className={`flex-1 ${textStyles}`}>{title}</span>
+         {rightIcon && (
+            <div className="relative w-6 h-6">
+               <Image src={rightIcon} alt="Right Icon" fill className="object-contain" />
+            </div>
+         )}
       </button>
    )
 }
