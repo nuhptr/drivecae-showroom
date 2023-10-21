a6739c6b1e5fd751479105e1e646e50d8b8e8e4d
nuhptr <nugrahaadi733@gmail.com>
2023-10-21 11:02:14 +0700

    update utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,40 @@
-import { CarProps, FilterProps } from "@/types"
+type ICarProps = {
+   city_mpg: number
+   class: string
+   combination_mpg: number
+   cylinders: number
+   displacement: number
+   drive: string
+   fuel_type: string
+   highway_mpg: number
+   make: string
+   model: string
+   transmission: string
+   year: number
+}
 
+type IFilterProps = {
+   manufacturer?: string
+   year?: number
+   model?: string
+   limit?: number
+   fuel?: string
+}
+
+// TASK: Fetch cars from the API
+export async function fetchCars() {
+   // Set the required headers for the API request
+   const headers: HeadersInit = {
+      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
+      "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+   }
+   const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla`, { headers: headers })
+
+   const result = await response.json()
+   return result
+}
+
+// TASK: Calculate the rental price per day
 export const calculateCarRent = (city_mpg: number, year: number) => {
    const basePricePerDay = 50 // Base rental price per day in dollars
    const mileageFactor = 0.1 // Additional rate per mile driven
@@ -15,51 +50,32 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
    return rentalRatePerDay.toFixed(0)
 }
 
+// TASK: Update search params in the URL
 export const updateSearchParams = (type: string, value: string) => {
    // Get the current URL search params
    const searchParams = new URLSearchParams(window.location.search)
-
    // Set the specified search parameter to the given value
    searchParams.set(type, value)
-
    // Set the specified search parameter to the given value
    const newPathname = `${window.location.pathname}?${searchParams.toString()}`
 
    return newPathname
 }
 
+// TASK: Delete search params from the URL
 export const deleteSearchParams = (type: string) => {
    // Set the specified search parameter to the given value
    const newSearchParams = new URLSearchParams(window.location.search)
-
    // Delete the specified search parameter
    newSearchParams.delete(type.toLocaleLowerCase())
-
    // Construct the updated URL pathname with the deleted search parameter
    const newPathname = `${window.location.pathname}?${newSearchParams.toString()}`
 
    return newPathname
 }
 
-export async function fetchCars(filters: FilterProps) {
-   const { manufacturer, year, model, limit, fuel } = filters
-
-   // Set the required headers for the API request
-   const headers: HeadersInit = {
-      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
-      "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-   }
-   // Set the required headers for the API request
-   const response = await fetch(
-      `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
-      { headers: headers }
-   )
-   // Parse the response as JSON
-   const result = await response.json()
-   return result
-}
-
-export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+// TASK: Fetch car details from the API
+export const generateCarImageUrl = (car: ICarProps, angle?: string) => {
    const url = new URL("https://cdn.imagin.studio/getimage")
    const { make, model, year } = car
 
