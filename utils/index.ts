9f0bcac1b52040171cd7a1a0d5d9c8f36f57ccc9
nuhptr <nugrahaadi733@gmail.com>
2023-10-22 19:27:41 +0700

    update utils

--- utils/index.ts
+++ utils/index.ts
@@ -28,10 +28,26 @@ export async function fetchCars() {
       "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
       "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
    }
-   const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla`, { headers: headers })
+   const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3`, { headers: headers })
 
    const result = await response.json()
    return result
+}
+
+// TASK: Fetch car details from the API
+export const generateCarImageUrl = (car: ICarProps, angle?: string) => {
+   const url = new URL("https://cdn.imagin.studio/getimage")
+   const { make, model, year } = car
+
+   url.searchParams.append("customer", process.env.NEXT_PUBLIC_IMAGIN_API_KEY || "")
+   url.searchParams.append("make", make)
+   url.searchParams.append("modelFamily", model.split(" ")[0])
+   url.searchParams.append("zoomType", "fullscreen")
+   url.searchParams.append("modelYear", `${year}`)
+   // url.searchParams.append('zoomLevel', zoomLevel);
+   url.searchParams.append("angle", `${angle}`)
+
+   return `${url}`
 }
 
 // TASK: Calculate the rental price per day
@@ -72,20 +88,4 @@ export const deleteSearchParams = (type: string) => {
    const newPathname = `${window.location.pathname}?${newSearchParams.toString()}`
 
    return newPathname
-}
-
-// TASK: Fetch car details from the API
-export const generateCarImageUrl = (car: ICarProps, angle?: string) => {
-   const url = new URL("https://cdn.imagin.studio/getimage")
-   const { make, model, year } = car
-
-   url.searchParams.append("customer", process.env.NEXT_PUBLIC_IMAGIN_API_KEY || "")
-   url.searchParams.append("make", make)
-   url.searchParams.append("modelFamily", model.split(" ")[0])
-   url.searchParams.append("zoomType", "fullscreen")
-   url.searchParams.append("modelYear", `${year}`)
-   // url.searchParams.append('zoomLevel', zoomLevel);
-   url.searchParams.append("angle", `${angle}`)
-
-   return `${url}`
 }
