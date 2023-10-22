9e6fe2f293e17564d0fada04c117ef3e08d171c4
nuhptr <nugrahaadi733@gmail.com>
2023-10-22 08:51:50 +0700

    update types

--- types/index.ts
+++ types/index.ts
@@ -1,30 +1,3 @@
-import { MouseEventHandler } from "react"
-
-export interface ICustomButtonProps {
-   isDisabled?: boolean
-   btnType?: "button" | "submit"
-   containerStyles?: string
-   textStyles?: string
-   title: string
-   rightIcon?: string
-   handleClick?: MouseEventHandler<HTMLButtonElement>
-}
-
-export interface ICarProps {
-   city_mpg: number
-   class: string
-   combination_mpg: number
-   cylinders: number
-   displacement: number
-   drive: string
-   fuel_type: string
-   highway_mpg: number
-   make: string
-   model: string
-   transmission: string
-   year: number
-}
-
 export interface IFilterProps {
    manufacturer?: string
    year?: number
@@ -37,32 +10,7 @@ export interface IHomeProps {
    searchParams: IFilterProps
 }
 
-export interface ICarCardProps {
-   model: string
-   make: string
-   mpg: number
-   transmission: string
-   year: number
-   drive: string
-   cityMPG: number
-}
-
-export interface IOptionProps {
-   title: string
-   value: string
-}
-
-export interface ICustomFilterProps {
-   title: string
-   options: IOptionProps[]
-}
-
 export interface IShowMoreProps {
    pageNumber: number
    isNext: boolean
-}
-
-export interface ISearchManuFacturerProps {
-   manufacturer: string
-   setManuFacturer: (manufacturer: string) => void
 }
