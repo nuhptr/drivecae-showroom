95e5ec01f76e8fbae40d62f043187f37debe2065
nuhptr <nugrahaadi733@gmail.com>
2023-10-27 10:37:47 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    images: {
-      remotePatterns: [{ protocol: "https", hostname: "cdn.imagin.studio", path: "" }],
+      remotePatterns: [{ protocol: "https", hostname: "cdn.imagin.studio" }],
    },
 }
 
